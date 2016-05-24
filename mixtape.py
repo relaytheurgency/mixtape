06390cd2d4e89c3ce781928b412adbceaf0ca0ae
Brandon Jones <brandon.jones@cerner.com>
2016-05-24 14:06:33 -0500

    Put functions in mixtape.py, get start of artist search function

--- mixtape.py
+++ mixtape.py
@@ -6,17 +6,34 @@ import internetarchive
 from random import randint
 import os
 
-mixtapes = []
 
-# create a list of all of the mixtapes available
-for i in internetarchive.search_items('collection:hiphopmixtapes'):
-    mixtapes.append(i['identifier'])
+def search_artist(artist_name):
+    '''Search archive.org for tapes from an artist'''    
+    artist_tapes = search_items('collection:hiphopmixtapes AND title:' + artist_name)
+    
+    for tape in artist_tapes:
+        print tape['identifier']
 
-# select a random mixtape
-mixtape = mixtapes[randint(0,len(mixtapes))]
+    return artist_tapes
 
-print mixtape
 
-command = "mplayer -msgcolor -msglevel all=0:demux=4:statusline=5 -playlist http://archive.org/download/" + mixtape + "/" + mixtape + "_vbr.m3u 2>/dev/null"
+def random_mixtape():
+    # create a list of all of the mixtapes available
 
-os.system(command)
+    mixtapes=[]
+
+    for i in internetarchive.search_items('collection:hiphopmixtapes'):
+        mixtapes.append(i['identifier'])
+
+    # select a random mixtape
+    mixtape = mixtapes[randint(0,len(mixtapes))]
+
+    return mixtape
+
+def play_tape(mixtape):
+
+    command = "mplayer -msgcolor -msglevel all=0:demux=5:statusline=5 -playlist http://archive.org/download/" + mixtape + "/" + mixtape + "_vbr.m3u 2>/dev/null"
+    os.system(command)
+
+
+play_tape(random_mixtape())
